9b74d868ad63f770b0556a815ff792632d6f1d69
italo <>
2020-06-19 10:05:52 -0300

    finalizacao do test de geracao de usuario

--- domain/user_test.go
+++ domain/user_test.go
@@ -12,7 +12,7 @@ func testNewUser(t *testing.T) {
 
 	_, err := domain.NewStructUser("Italo", "italo@gmail.com", "123")
 	require.Nil(t, err)
-
+	// testa se a validacao do email estar correta
 	_, err2 := domain.NewStructUser("Maria", "maria@gmail.com", "123")
-	require.Nil(t, err2)
+	require.Error(t, err2)
 }
